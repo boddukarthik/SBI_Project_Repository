041018658bfd2a152c4e7fe98a4b2d8c53c82ab9
boddukarthik <kiran.boddu24@gmail.com>
2020-07-31 20:07:16 +0530

    zuul server pushed to master branch

---
+++ ZuualServer/src/main/java/com/zuulserver/ZuualServer/ZuualServerApplication.java
@@ -0,0 +1,18 @@
+package com.zuulserver.ZuualServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@SpringBootApplication
+@EnableEurekaClient 
+@EnableZuulServer
+public class ZuualServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuualServerApplication.class, args);
+	}
+
+}
---
+++ ZuualServer/src/test/java/com/zuulserver/ZuualServer/ZuualServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuulserver.ZuualServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuualServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
